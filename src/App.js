3d160c1c940730283063fed1336dcc8d2c0d1738
Kerry Murphy <kerryme2@hotmail.com>
2023-04-16 18:42:36 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Kerry Murphy and is open source on{" "}
-        <a
-          href="https://github.com/Kerrigan1337/react-weather-app"
-          target="_blank"
-          without
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Kerry Murphy and is open source on{" "}
+          <a
+            href="https://github.com/Kerrigan1337/react-weather-app"
+            target="_blank"
+            without
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

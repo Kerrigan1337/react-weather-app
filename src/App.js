3b53ba113ff2db27ff7b4f31cbe84e0fc73d0b5e
Kerry Murphy <kerryme2@hotmail.com>
2023-04-16 18:24:25 +0100

    Added Github link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Kerry Murphy and is open source on{" "}
+        <a
+          href="https://github.com/Kerrigan1337/react-weather-app"
+          target="_blank"
+          without
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
